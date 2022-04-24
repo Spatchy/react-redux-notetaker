f75ca4eec5c914418516f37249dc3db50b8623e1
Spatchy <spatchy@spatchy.net>
2022-04-24 15:28:54 +0100

    imported bulma and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import 'bulma/css/bulma.min.css'
 import Navbar from './components/Navbar'
+import NotesHome from './views/NotesHome'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <NotesHome></NotesHome>
     </div>
   );
 }
