b8c572e2f50eb126353ccd7644112a676b724652
Spatchy <spatchy@spatchy.net>
2022-04-24 23:46:23 +0100

    testing views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import 'bulma/css/bulma.min.css'
+import 'bulma/css/bulma.min.css' // Quickly use Bulma with no customisations
+import './assets/fontawesome/solid.min.css'
+import './assets/fontawesome/fontawesome.min.css'
+
 import Navbar from './components/Navbar'
-import NotesHome from './views/NotesHome'
+//import NotesHome from './views/NotesHome'
+//import Landing from './views/Landing'
+import Signup from './views/Signup'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <NotesHome></NotesHome>
+      {/* <NotesHome></NotesHome> */}
+      {/* <Landing></Landing> */}
+      <Signup></Signup>
     </div>
   );
 }
