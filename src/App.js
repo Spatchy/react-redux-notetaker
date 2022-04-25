1948ad7bdc5e027572844d7b47f676465495fa40
Spatchy <spatchy@spatchy.net>
2022-04-25 18:12:36 +0100

    connected redux store

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './assets/fontawesome/solid.min.css'
 import './assets/fontawesome/fontawesome.min.css'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 
 import Navbar from './components/Navbar'
 import NotesHome from './views/NotesHome'
@@ -12,14 +15,16 @@ import Signup from './views/Signup'
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element = {<NotesHome />} />
-          <Route path="/login" element = {<Login />} />
-          <Route path="/signup" element = {<Signup />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element = {<NotesHome />} />
+            <Route path="/login" element = {<Login />} />
+            <Route path="/signup" element = {<Signup />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   );
 }
