a377919c5ed781d28f9dd6bbb26212c325d69d20
Spatchy <spatchy@spatchy.net>
2022-04-26 16:20:14 +0100

    created firestore connection

---
+++ src/firestore/firestore.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs, addDoc, updateDoc } from 'firebase/firestore/lite'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAteJO-409tWwUuAH0EsJSyfrCyv0svjQ",
+  authDomain: "react-redux-notetaker.firebaseapp.com",
+  projectId: "react-redux-notetaker",
+  storageBucket: "react-redux-notetaker.appspot.com",
+  messagingSenderId: "60630492830",
+  appId: "1:60630492830:web:a405cd40e61e0a748dbd8f"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export default {db, collection, getDocs, addDoc, updateDoc}
