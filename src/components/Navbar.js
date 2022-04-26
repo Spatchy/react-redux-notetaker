d84e975dafe52ae74919a67712256bd05526818f
Spatchy <spatchy@spatchy.net>
2022-04-26 22:47:37 +0100

    implemented save and logout buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react"
+import { connect } from "react-redux"
+import saveNotes from "../firestore/saveNotes"
 import logo from "../logo.svg"
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+  const save = () => {
+    const attempt = saveNotes(props.user.email, props.user.notes)
+    if(attempt instanceof Error) {
+      props.setError(attempt.message)
+    }
+  }
+
+  const logout = () => {
+    props.logOut()
+  }
+
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
@@ -26,12 +40,25 @@ const Navbar = () => {
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              <a className="button is-primary" href="/signup">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light" href="login">
-                Log in
-              </a>
+              {props.user.isAuthenticated ? (
+                <>
+                  <button className="button is-primary" onClick={() => {save()}}>
+                    <strong>Save Changes</strong>
+                  </button>
+                  <button className="button is-danger" onClick={() => {logout()}}>
+                    Log Out
+                  </button>
+                </>
+              ) : (
+                <>  
+                  <a className="button is-primary" href="/signup">
+                    <strong>Sign Up</strong>
+                  </a>
+                  <a className="button is-light" href="login">
+                    Log in
+                  </a>
+                </>
+              )}
             </div>
           </div>
         </div>
@@ -40,4 +67,26 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+  return {
+    user: state.reducer.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setError: (error) => {
+      dispatch({
+        type: "SET_ERROR",
+        payload: error
+      })
+    },
+    logOut: () => {
+      dispatch({
+        type: "LOG_OUT"
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
