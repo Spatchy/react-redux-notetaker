a33024d1468689260febdd543ca9b3f657326da7
Spatchy <spatchy@spatchy.net>
2022-04-26 23:14:02 +0100

    fixed and tweaked navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,23 +19,12 @@ const Navbar = (props) => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
+        <a className="navbar-item" href="https://github.com/Spatchy/react-redux-notetaker">
           <img src={logo} width="40" height="40" alt=""></img>
         </a>
-
-        <button className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </button>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item" href="/">
-            Home
-          </a>
-        </div>
+      <div className="navbar-menu">
 
         <div className="navbar-end">
           <div className="navbar-item">
@@ -54,7 +43,7 @@ const Navbar = (props) => {
                   <a className="button is-primary" href="/signup">
                     <strong>Sign Up</strong>
                   </a>
-                  <a className="button is-light" href="login">
+                  <a className="button is-light" href="/login">
                     Log in
                   </a>
                 </>
@@ -83,7 +72,7 @@ const mapDispatchToProps = (dispatch) => {
     },
     logOut: () => {
       dispatch({
-        type: "LOG_OUT"
+        type: "LOGOUT"
       })
     }
   }
